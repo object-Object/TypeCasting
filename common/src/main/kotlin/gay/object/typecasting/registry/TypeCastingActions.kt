2f55784927583783c303ce66eec7c91f735682f3
object-Object <object@objectobject.ca>
2026-07-11 21:22:30 -0400

    Change pattern

--- common/src/main/kotlin/gay/object/typecasting/registry/TypeCastingActions.kt
+++ common/src/main/kotlin/gay/object/typecasting/registry/TypeCastingActions.kt
@@ -20,12 +20,7 @@ object TypeCastingActions : TypeCastingRegistrar<ActionRegistryEntry>(
         OpCompileSubroutine,
     )
 
-    val SET_OP_LIMIT_TRAP = make(
-        "set_op_limit_trap",
-        HexDir.SOUTH_EAST,
-        "wdwewawqwqw",
-        OpSetOpLimitTrap,
-    )
+    val SET_OP_LIMIT_TRAP = make("set_op_limit_trap", HexDir.SOUTH_EAST, "weedqwa", OpSetOpLimitTrap)
 
     private fun make(name: String, startDir: HexDir, signature: String, action: Action) =
         make(name, startDir, signature) { action }
